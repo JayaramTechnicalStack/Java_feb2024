e34d04e7aebecabd18f6027057d0d8ad4769da8c
manu <pmanu6280@gmail.com>
2024-06-05 21:04:56 +0530

    show method added

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 public class Test {
 
+public void show(){
+System.out.println("Welcome to java");
+
+}
+
+
 public static void main(String [] args){
 
 System.out.println("Welcome to GitHub concept");
